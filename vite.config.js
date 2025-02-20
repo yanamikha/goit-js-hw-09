1f29f30a5d4a0baafa9e946544cf0e5e5cf573a6
yanamikha <yanamikha@gmail.com>
2025-02-20 01:01:23 +0100

    remove postcss-sort-media-queries

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
